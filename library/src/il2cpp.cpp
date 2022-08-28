66e838d6e6d89698a74b7bdfed77d3f9ae9e236f
lanylow <lanylow@protonmail.com>
2022-08-28 20:24:48 +0200

    Updated to game version 3.0.

--- library/src/il2cpp.cpp
+++ library/src/il2cpp.cpp
@@ -5,9 +5,9 @@ namespace GenshinUtility {
     while (!(m_unityPlayer = GetModuleHandleA("UnityPlayer.dll")))
       Sleep(100);
   
-    m_setFieldOfView = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0xb6b920);
-    m_targetFrameRate = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0x1b31724);
-    m_setVsyncCount = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0x1136e80);
-    m_setFog = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0x11338d0);
+    m_setFieldOfView = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0xbaa120); // "Cannot set field of view on this camera while VR is enabled." -> xref calling function that uses jmp
+    m_targetFrameRate = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0x1b90934); // 8B 05 ? ? ? ? 66 0F 6E C8 0F 5B C9 EB -> variable referenced in mov
+    m_setVsyncCount = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0x1188340); // 48 83 EC 28 48 63 41 -> xref calling function 
+    m_setFog = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(m_unityPlayer) + 0x118ae00); // E8 ? ? ? ? 48 8B 75 C8 48 8B 4D -> relative call -> xref calling function that uses jmp
   }
 }
