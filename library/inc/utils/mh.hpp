85c1b8394f6c5164aa048ff8ac9cdd8c325d662e
lanylow <lanylow@protonmail.com>
2023-04-21 16:01:28 +0200

    More strict types for the minhook wrapper.

--- library/inc/utils/mh.hpp
+++ library/inc/utils/mh.hpp
@@ -5,12 +5,14 @@ namespace utils::mh {
     MH_Initialize();
   }
 
-  template <typename target_type, typename detour_type, typename original_type>
+  template <typename target_type, typename detour_type, typename original_type,
+    std::enable_if_t<std::is_pointer_v<detour_type> && std::is_pointer_v<original_type>, int> = 0>
   inline void hook(target_type target, detour_type detour, original_type original) {
     MH_CreateHook((void*)(target), (void*)(detour), (void**)(original));
   }
 
-  template <typename detour_type, typename original_type>
+  template <typename detour_type, typename original_type,
+    std::enable_if_t<std::is_pointer_v<detour_type> && std::is_pointer_v<original_type>, int> = 0>
   inline void hook_kiero(int index, detour_type detour, original_type original) {
     MH_CreateHook((void*)(kiero::getMethodsTable()[index]), (void*)(detour), (void**)(original));
   }
