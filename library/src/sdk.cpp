71c217196ba958b68cb8a7ee75bceeff7958b143
lanylow <lanylow@protonmail.com>
2023-12-20 14:53:02 +0100

    Updated for Genshin Impact 4.3.0.

--- library/src/sdk.cpp
+++ library/src/sdk.cpp
@@ -5,14 +5,14 @@
 void sdk::initialize_genshin_impact() {
   auto unity_player = (unsigned long long)(GetModuleHandleA("UnityPlayer.dll"));
 
-  sdk::set_field_of_view = unity_player + 0xd2dee0;
+  sdk::set_field_of_view = unity_player + 0xd260c0;
 
-  sdk::set_target_frame_rate = unity_player + 0xab1150;
-  sdk::quit = unity_player + 0xd27ef0;
+  sdk::set_target_frame_rate = unity_player + 0xabac70;
+  sdk::quit = unity_player + 0xd1cfe0;
 
-  sdk::set_vsync_count = unity_player + 0x13abfc0;
+  sdk::set_vsync_count = unity_player + 0x13add40;
 
-  sdk::set_fog = unity_player + 0x13a77c0;
+  sdk::set_fog = unity_player + 0x13afe20;
 
   sdk::game = sdk::game_type::genshin_impact;
 }
