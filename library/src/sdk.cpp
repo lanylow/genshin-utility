56ac64ad1f23a4dc7bb37cd2d77b21f60d153f9e
lanylow <lanylow@protonmail.com>
2024-09-10 23:12:20 +0200

    Updated for Honkai: Star Rail 2.5.0.

--- library/src/sdk.cpp
+++ library/src/sdk.cpp
@@ -35,15 +35,15 @@ void sdk::initialize_genshin_impact_chinese() {
 void sdk::initialize_star_rail() {
   auto game_assembly = (unsigned long long)(GetModuleHandleA("GameAssembly.dll"));
 
-  sdk::set_field_of_view = game_assembly + 0xb71e690;
+  sdk::set_field_of_view = game_assembly + 0x65b8110;
 
-  sdk::set_target_frame_rate = game_assembly + 0x533a540;
-  sdk::quit = game_assembly + 0x533a040;
+  sdk::set_target_frame_rate = game_assembly + 0x6870da0;
+  sdk::quit = game_assembly + 0x68708a0;
 
-  sdk::set_vsync_count = game_assembly + 0x53de090;
+  sdk::set_vsync_count = game_assembly + 0x696dc50;
 
-  sdk::enter = game_assembly + 0x5746fb0;
-  sdk::leave = game_assembly + 0x5749a50;
+  sdk::enter = game_assembly + 0x7609fc0;
+  sdk::leave = game_assembly + 0x760cb50;
 
   sdk::game_t::set(sdk::game_t::star_rail);
 }
