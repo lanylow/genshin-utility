3c0a669419a9d01b2d769f60c97c9de9b146bd15
lanylow <lanylow@protonmail.com>
2024-10-09 15:06:18 +0200

    Updated for Genshin Impact 5.1.0.

--- library/src/sdk.cpp
+++ library/src/sdk.cpp
@@ -5,14 +5,14 @@
 void sdk::initialize_genshin_impact_global() {
   auto mod = (unsigned long long)(GetModuleHandleA(nullptr));
 
-  sdk::set_field_of_view = mod + 0x165a1d0;
+  sdk::set_field_of_view = mod + 0x1136f30;
 
-  sdk::set_target_frame_rate = mod + 0x185be40;
-  sdk::quit = mod + 0x165e590;
+  sdk::set_target_frame_rate = mod + 0x1339720;
+  sdk::quit = mod + 0x113b3d0;
 
-  sdk::set_vsync_count = mod + 0xf79000;
+  sdk::set_vsync_count = mod + 0xa50430;
 
-  sdk::set_fog = mod + 0xf780b0;
+  sdk::set_fog = mod + 0xa4f4e0;
 
   sdk::game_t::set(sdk::game_t::genshin_impact);
 }
@@ -20,14 +20,14 @@ void sdk::initialize_genshin_impact_global() {
 void sdk::initialize_genshin_impact_chinese() {
   auto mod = (unsigned long long)(GetModuleHandleA(nullptr));
 
-  sdk::set_field_of_view = mod + 0x165f1d0;
+  sdk::set_field_of_view = mod + 0x1136d30;
 
-  sdk::set_target_frame_rate = mod + 0x1860e40;
-  sdk::quit = mod + 0x1663590;
+  sdk::set_target_frame_rate = mod + 0x1339520;
+  sdk::quit = mod + 0x113b1d0;
 
-  sdk::set_vsync_count = mod + 0xf7e000;
+  sdk::set_vsync_count = mod + 0xa50230;
 
-  sdk::set_fog = mod + 0xf7d0b0;
+  sdk::set_fog = mod + 0xa4f2e0;
 
   sdk::game_t::set(sdk::game_t::genshin_impact);
 }
