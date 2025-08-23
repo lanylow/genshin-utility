94cd23795c4d82c7ab945896db5f09cce8b376c5
lanylow <lanylow@protonmail.com>
2025-08-23 22:01:49 +0200

    Make the loader require administrator privileges.

--- src/loader/premake5.lua
+++ src/loader/premake5.lua
@@ -6,3 +6,19 @@ project "Loader"
   files { "src/**.*" }
 
   targetname "loader"
+
+  local function set_uac()
+    require("vstudio")
+
+    premake.override(premake.vstudio.vc2010.elements, "link", function(base, prj)
+      local calls = base(prj)
+
+      table.insert(calls, function()
+        premake.vstudio.vc2010.element("UACExecutionLevel", nil, "requireAdministrator")
+      end)
+
+      return calls
+    end)
+  end
+
+  set_uac()
